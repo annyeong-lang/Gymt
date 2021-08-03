2bc7afcd9df14d4dbecd67cc74cc30535a5b1e81
mallang7 <seonug0725@gmail.com>
2021-08-03 20:50:00 +0900

    search

--- screens/Search/Search.js
+++ screens/Search/Search.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Athlete from "./Athlete";
+import Location from "./Location";
+import SearchButt from "./SearchButt";
+import ScrollContainer from "../../components/ScrollContainer";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white'
+  }
+});
 
 export default () => (
-  <View>
-    <Text>Search</Text>
-  </View>
+  <ScrollContainer>
+    <Text>체육관 찾기</Text>
+    <Athlete></Athlete>
+    <Location></Location>
+    <SearchButt></SearchButt>
+  </ScrollContainer>
 );
